ede652585f07d69778a9a57df18baced33c3300c
Corbin <corbin.graham2@gmail.com>
2018-04-15 17:26:19 -0700

    pushing for jake

--- scripts/enqueue_episode.py
+++ scripts/enqueue_episode.py
@@ -1,6 +1,18 @@
-def add_to_queue(message, split_command):
+def build_message(message, split_command):
 	try:
 	  msg = message.author.mention + ' has enqueued ' + split_command[1].format(message)
 	except IndexError:
 	  msg = 'BAKA! I need something to enqueue!'
 	return msg
+
+def add_to_queue(message, split_command):
+	return find_caller(message)
+
+def find_caller(message):
+	caller = message.author.voice.voice_channel
+	if caller:
+		find_users(message)
+	else:
+		return 'You need to be in a channel'
+
+# def find_users(message):
