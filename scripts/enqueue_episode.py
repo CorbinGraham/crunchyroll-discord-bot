80b39977291c79d1ec99889e0bd823733a65b44a
Corbin <corbin.graham2@gmail.com>
2018-04-15 17:37:19 -0700

    simplifying the enqueuing

--- scripts/enqueue_episode.py
+++ scripts/enqueue_episode.py
@@ -1,18 +1,16 @@
+def add_to_queue(message, split_command):
+	return find_caller(message, split_command)
+
+def find_caller(message, split_command):
+	caller = message.author.voice.voice_channel
+	if caller:
+		return build_message(message, split_command)
+	else:
+		return 'You need to be in a channel'
+
 def build_message(message, split_command):
 	try:
 	  msg = message.author.mention + ' has enqueued ' + split_command[1].format(message)
 	except IndexError:
 	  msg = 'BAKA! I need something to enqueue!'
 	return msg
-
-def add_to_queue(message, split_command):
-	return find_caller(message)
-
-def find_caller(message):
-	caller = message.author.voice.voice_channel
-	if caller:
-		find_users(message)
-	else:
-		return 'You need to be in a channel'
-
-# def find_users(message):
