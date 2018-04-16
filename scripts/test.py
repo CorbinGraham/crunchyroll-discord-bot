50d23ab56d9c73f9cead28f213c28283189fb5ba
Jake Klan <jacobklan@gmail.com>
2018-04-15 18:39:55 -0700

    Added ability for users to watch shows together

--- scripts/test.py
+++ scripts/test.py
@@ -1,8 +1,10 @@
 # https://github.com/Rapptz/discord.py/blob/async/examples/reply.py
 import discord
-import enqueue_episode
+import watch_together
+import os
+import asyncio
 
-TOKEN = shit
+TOKEN =  str(os.environ['002TOKEN'])
 
 client = discord.Client()
 
@@ -12,14 +14,28 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('!hello'):
+    if message.content.startswith('$hello'):
         msg = 'Hello {0.author.mention}'.format(message)
         await client.send_message(message.channel, msg)
 
-    if message.content.startswith('!queue'):
-        msg = enqueue_episode.add_to_queue(message, message.content.split(" "))
-        connected_users = enqueue_episode.find_users(message)
+    if message.content.startswith('$watch'):
+        msg = watch_together.add_to_queue(message, message.content.split(" "))
+        connected_users = watch_together.find_users(message)
+
         await client.send_message(message.channel, msg)
+
+        msg = watch_together.send_ready_message()
+        ready_message = await client.send_message(message.channel, msg)
+        await client.add_reaction(ready_message, "✅")
+
+        ready = 0
+        while(ready < connected_users):
+            await client.wait_for_reaction("✅")
+            print ('user readied!')
+            ready += 1
+
+        await client.send_message(message.channel, '3, 2, 1, Start!', tts=True)
+
 
 @client.event
 async def on_ready():
